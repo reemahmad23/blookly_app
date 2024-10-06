f953ba5adbe0cee5eb4fdf93cef1b89328e00e94
reemahmed <reemmahmed40@gmail.com>
2024-10-06 14:44:44 +0200

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    
+  }
+  }
