262c766a371a052f7ae923a4a98a64159b06d6a2
reemahmed <reemmahmed40@gmail.com>
2024-10-07 15:27:19 +0200

    best seller list part1

--- lib/features/home/data/presentation/view_model/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/view_model/views/widgets/home_view_body.dart
@@ -27,6 +27,42 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.titleNedium,
             ),
+            BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+          aspectRatio: 2.5 / 4,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(AssetsData.testImage
+                ),
+                ),
+            ),
+          ),
+        
+      ),
+          Column(
+            children: [
+      
+            ],
+          )
         ],
       ),
     );
