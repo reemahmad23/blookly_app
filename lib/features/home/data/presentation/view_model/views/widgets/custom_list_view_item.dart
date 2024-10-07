5cd862fd95e1b34aa02eb8293f3fd2c3a33c2ea9
reemahmed <reemmahmed40@gmail.com>
2024-10-07 12:43:24 +0200

    add featured list view to home

---
+++ lib/features/home/data/presentation/view_model/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,25 @@
+
+import 'package:bookly_app/core/utilities/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(AssetsData.testImage
+              ),
+              ),
+          ),
+        ),
+      
+    );
+  }
+}
--- lib/features/home/data/presentation/view_model/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/view_model/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utilities/assets.dart';
 import 'package:bookly_app/features/home/data/presentation/view_model/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/data/presentation/view_model/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,33 +14,28 @@ class HomeViewBody extends StatelessWidget {
       children: [
         const CustomAppBar(),
 
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
-
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
           height: MediaQuery.of(context).size.height * .23,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(AssetsData.testImage
-              ),
-              ),
-          ),
-        ),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: CustomListViewItem(),
+            );
+        }
       ),
     );
+    }
   }
-}
